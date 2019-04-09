685d988f291894ddf8798125648e4e1de847627b
Shama Ahlawat <shama@oriserve.com>
2019-04-09 15:35:27 +0530

    emitting messsages

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var socket = require('socket.io')
 //App setup
 
 var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening to request on port 4000')
+var server = app.listen(5000, function(){
+    console.log('listening to request on port 5000')
 })
 
 //static files
@@ -16,4 +16,8 @@ var io = socket(server); // sockets on server side
 
 io.on('connection' , function(socket){ //when connect is detected then callback function which takes info about my 
     console.log('made socket connection',socket.id) //everytime we refresh all unique socket connection b/w client and server
-})
+
+    socket.on('chat',function(data){ 
+       io.sockets.emit('chat', data);
+    })
+});
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,26 @@
 //make connection
 //frontend socket
 var socket = io.connect('http://localhost:4000')
+
+//query DOM
+var message = document.getElementById('message')
+var handle = document.getElementById('handle')
+var btn = document.getElementById('send')
+var output = document.getElementById('output');
+
+//emit events when someone clicks on send
+
+btn.addEventListner('click',function() {
+    console.log('triggered')
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
+
+//listen for events on frontend
+
+socket.on('chat',function(data){
+    output.innerHTML += '<p><strong>'+ data.handle +
+    ':</strong' + data.message + '</p>';
+})
